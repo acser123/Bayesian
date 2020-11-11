41995dbd5bf73eec4f496cf5ed30d934699fa7b4
Andras Cser <acser@yahoo.com>
2020-11-11 17:54:43 -0500

    count not working properly, have to fix

--- nbc.c
+++ nbc.c
@@ -70,6 +70,18 @@ list_node *create_new_list_node (const char *label)
         return NULL;
     }
     strcpy (new_node->label, label);  /* copy data to new_node->data */
+
+
+    /* allocate/validate storage for count */
+
+  //  if (!(new_node->count = malloc (sizeof (int) + 1))) {
+   //     perror ("malloc-new_node->count");
+    //    free (new_node);
+     //   return NULL;
+    //}
+    new_node->count = 0;  /* initialize count to 0*/
+
+
     new_node->next = NULL;          /* set next pointer NULL */
 
     return new_node;    /* return pointer to new_node */
@@ -104,9 +116,9 @@ void print_list (list *list_in)
     while (current != NULL)
     {
         if (current == list_in->head)
-            printf ("%s", current->label);
+            printf ("%s count=%d", current->label, current->count);
         else
-            printf (",%s", current->label);
+            printf (",%s count=%d", current->label, current->count);
         current = current->next;
     }
 
@@ -240,6 +252,7 @@ void cross_link_nodes(table *table_in) {
         list *next_list_ptr = curr_list_ptr->next;
         if (next_list_ptr != NULL) {
             list_node *curr_node_ptr = curr_list_ptr->head;
+/* This list points to the one below curr_nod_ptr */
             list_node *below_node_ptr = next_list_ptr->head;
             while (below_node_ptr!= NULL) {
 
@@ -254,6 +267,7 @@ void cross_link_nodes(table *table_in) {
             }
             
         }
+        /* Move to the next line */ 
         curr_list_ptr = curr_list_ptr->next;
     }  
 }
@@ -290,25 +304,65 @@ list * retrieve_column (table *table_in, char *header_in)
      return list_out; 
 }
 
-void get_unique_counts(list *list_in, table *table_in) {
+list * get_uniques(list *list_in) {
 
-    list *current_list = table_in->head;
-    int i;
-    while (current_list != NULL)
-    {
-	i++;
-
-        list_node *current_node = current_list->head;
-
-        while (current_node != NULL) {
-            //printf ("%s,", current_node->label);
-            current_node = current_node->next;
-        }
-       // printf ("\n");
-
-        current_list = current_list->next;
+    list *unique_list_out = new_list();
    
+    list_node *curr_node = list_in->head;
+
+
+    /* The first node label is unique for sure so we initialize the unique list */
+    list_node *unique_list_node = create_new_list_node(curr_node->label);
+    unique_list_node->count++;
+    insert_node_at_end(unique_list_out, unique_list_node);
+    
+    /* Step to the second item on the current node */
+    curr_node = curr_node->next; 
+
+    int numrows=1;
+    while (curr_node != NULL) {
+        printf("*debug* get_uniques(): evaluation curr_node->label=%s; numrows=%d\n", curr_node->label, numrows);
+        numrows++;
+        /* Go the beginning of the unique list */
+        unique_list_node = unique_list_out->head;
+        int found=0;
+        //int i;
+        list_node *add_node;
+        /* Step through unique list */
+        while (unique_list_node != NULL) { 
+         //   printf("*debug* get_uniques(): in unique while(), equal unique_list_node->label=%s; curr_node->label=%s\n", unique_list_node->label, curr_node->label);
+            /* If unique list contains the current node, set found to 1 
+               so that we don't add it after having finished with the unique list */
+            if ((strcmp(unique_list_node->label, curr_node->label) == 0)) {
+            //    printf("*debug* get_uniques(): inner while(), equal unique_list_node->label=%s; curr_node->label=%s\n", unique_list_node->label, curr_node->label);
+                add_node = create_new_list_node(curr_node->label);
+                unique_list_node->count++;
+                printf("*debug* get_uniques(): inner while(), row=%d; equal unique_list_node->label=%s; curr_node->label=%s; unique_list_node->count=%d;\n", numrows, unique_list_node->label, curr_node->label, unique_list_node->count);
+                found = 1;
+            } else {
+
+                add_node = create_new_list_node(curr_node->label);
+            }
+            unique_list_node = unique_list_node->next;
+        //printf("*debug* get_uniques(): unique_list="); print_list(unique_list_out);
+        }
+        //printf("*debug* get_uniques(): inner while() finished, found=%d, add_node->label=%s\n", found, add_node->label);
+        if (found==0)  { 
+             //add_node->count = i;
+             insert_node_at_end(unique_list_out, add_node);
+        
+            //printf("*debug* get_uniques(): inserted node at end of uniques add_node->label=%s\n", add_node->label);
+        //printf("*debug* get_uniques(): after insert unique_list="); print_list(unique_list_out);
+            found=1;
+        } 
+
+
+        curr_node = curr_node->next; 
+        //printf("*debug* get_uniques(): next curr_node\n\n");
     }
+
+
+    return unique_list_out;
 }
 
 
@@ -393,18 +447,36 @@ int main (int argc, char *argv[]) {
     /* Cross link nodes in the training table so we can walk it vertically as well*/
     cross_link_nodes(training_table); 
     list *testcolumn = new_list();
-    testcolumn = retrieve_column(training_table, "1Outlook");
+    testcolumn = retrieve_column(training_table, "Outlook");
     print_list(testcolumn);
-    testcolumn = retrieve_column(training_table, "1Play");
+    list *counts_list = new_list();
+    counts_list = get_uniques(testcolumn);
+    print_list(counts_list);
+/*
+    testcolumn = retrieve_column(training_table, "Humidity");
     print_list(testcolumn);
+    counts_list = new_list();
+    counts_list = get_uniques(testcolumn);
+    print_list(counts_list);
 
+    testcolumn = retrieve_column(training_table, "Wind");
+    print_list(testcolumn);
+    counts_list = new_list();
+    counts_list = get_uniques(testcolumn);
+    print_list(counts_list);
+
+    testcolumn = retrieve_column(training_table, "Play");
+    print_list(testcolumn);
+    counts_list = new_list();
+    counts_list = get_uniques(testcolumn);
+    print_list(counts_list);
+*/
 
     /* Read and analyze all training table values */
     table *parameter_table = new_table();
     list *Ci = new_list();
     insert_list_at_end(parameter_table, Ci);
 
-    //get_unique_counts(Ci, training_table);
 
     /* Identify unique values of the last column, C  or Class */
     /* Count unique values of the last column, count(Ci) */
